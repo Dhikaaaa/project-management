5ca0b01e09bc21d8b54cbd82e4e4f0df1bb080d0
dhikaaaa <dhikabahri4@gmail.com>
2022-05-27 17:36:34 +0700

    Add migrations, models, and controllers

---
+++ app/Models/Project.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    /**
+     * Get the clients that owns the project.
+     */
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    /**
+     * Get the categories that owns the project.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Get the tasks that owns the project.
+     */
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Client;
+use App\Models\Project;
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +19,57 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'dhikaa',
+            'email' => 'dhikabahri4@gmail.com',
+            'password' => bcrypt('akudhika123'),
+        ]);
+
+        Category::create([
+            'category' => 'Web Development'
+        ]);
+
+        Category::create([
+            'category' => 'Design Graphic'
+        ]);
+
+        Category::create([
+            'category' => 'Bussines Marketing'
+        ]);
+
+        Client::create([
+            'name' => 'Yofan Niki Andara',
+            'address' => 'Cluring, Banyuwangi',
+            'contact' => '087857875680',
+            'profil' => 'Chief Technology Officer',
+            'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet illum dicta velit rerum, autem ea laborum aliquam similique corporis enim.'
+        ]);
+
+        Project::create([
+            'name' => 'CV Jayla Tech',
+            'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet illum dicta velit rerum, autem ea laborum aliquam similique corporis enim. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet illum dicta velit rerum, autem ea laborum aliquam similique corporis enim.',
+            'progress' => 20,
+            'due_date' => date('2022-05-25'),
+            'status' => 'On Progress',
+            'category_id' => 1,
+            'client_id' => 1
+        ]);
+
+        Task::create([
+            'task' => 'Make Diagram Activity',
+            'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+            'due_date' => date('2022-05-25'),
+            'status' => 'Done',
+            'project_id' => 1
+        ]);
+
+        Task::create([
+            'task' => 'Make Entity Relational Diagram',
+            'desc' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+            'due_date' => date('2022-05-25'),
+            'status' => 'On Progress',
+            'project_id' => 1
+        ]);
     }
 }
